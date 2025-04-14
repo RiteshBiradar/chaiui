789f8cd4795ce7411c90c754d699ecbad83e89d4
Muksiduz <muksiduzzaman124@gmail.com>
2025-04-14 18:12:56 +0530

    solving the issue of shadcn - muksid

---
+++ registry/registry-components.ts
@@ -0,0 +1,13 @@
+import { Registry } from "./schema";
+
+export const ui: Registry=[
+    {
+        name:"basic-button",
+        type: "registry:block",
+        registryDependencies:[],
+        files:[
+            "ui/buttons/basicButton.tsx"
+        ]
+
+    }
+]
---
+++ registry/ui/buttons/basicButton.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
+
+// 1. Define variants using cva
+const buttonVariants = cva(
+  "px-8 py-3 cursor-pointer text-white font-bold rounded-lg shadow-md transition transform duration-300 hover:-translate-y-1 hover:scale-110 hover:shadow-xl",
+  {
+    variants: {
+      variant: {
+        default: "bg-gray-600",
+        destructive: "bg-red-800",
+        primary: "bg-teal-600",
+      },
+      size: {
+        default: "h-12",
+        md: "h-24",
+        lg: "h-53",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+// 2. Define props type using VariantProps + button props
+type AllButtonsProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>;
+
+// 3. Create a reusable button
+export default function AllButtons({
+  variant,
+  size,
+  className,
+  ...props
+}: AllButtonsProps) {
+  return (
+    <button
+      {...props}
+      className={twMerge(clsx(buttonVariants({ variant, size }), className))}
+    />
+  );
+}
