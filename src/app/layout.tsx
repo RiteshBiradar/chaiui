8672f96f57d4a6e888b754da82c2d1e203523efd
Muksiduz <muksiduzzaman124@gmail.com>
2025-04-17 09:37:50 +0530

    added Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
 import { Inter } from 'next/font/google';
 import type { ReactNode } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({
   subsets: ['latin'],
@@ -13,6 +14,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
+      <Analytics/>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
