fdb9970c874a9becdb00886166e7f623298cd400
Muksiduz <muksiduzzaman124@gmail.com>
2025-04-20 12:54:13 +0530

    vercel error of address

--- src/app/components/ChaiThemed/ShowSoloCard.jsx
+++ src/app/components/ChaiThemed/ShowSoloCard.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import SoloCountCard from '../../../../registry/ui/ChaiThemed/soloCountCard';
+import React from "react";
+import SoloCountCard from "../../../../registry/ui/chaiThemed/soloCountCard";
 
 function ShowSimpleCard() {
   return (
     <div className="flex justify-center items-center gap-4  flex-wrap">
-        <SoloCountCard label='Chai Lovers'/>
+      <SoloCountCard label="Chai Lovers" />
     </div>
-  )
+  );
 }
 
-export default ShowSimpleCard
+export default ShowSimpleCard;
